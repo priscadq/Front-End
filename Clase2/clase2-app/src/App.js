182eee2e1681d5e54b5ae81d59549ed2cd5b3593
priscadq <priscadq@gmail.com>
2018-01-13 11:46:38 -0300

    test

--- Clase2/clase2-app/src/App.js
+++ Clase2/clase2-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Student from "./Student";
 
 const estudiante = {
   firstName: "Priscila",
@@ -18,11 +19,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-
-        <p>
-          {firstName}
-          {lastName}
-        </p>
+        <Student firstName={estudiante && estudiante.firstName} />
 
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
--- Clase2/clase2-app/src/Student.js
+++ Clase2/clase2-app/src/Student.js
@@ -2,14 +2,14 @@ import React, { Component } from "react";
 
 class Student extends Component {
   render() {
-    const { firstName, lastName } = this.props;
+    const { firstName, lastName, dni } = this.props;
     console.log(firstName);
 
     return (
-      <p1>
-        <h1>{firstName}</h1>
-        <h2>Probando</h2>
-      </p1>
+      <p>
+        {firstName} test
+        {lastName}
+      </p>
     );
   }
 }

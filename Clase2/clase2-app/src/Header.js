5656418d0e66969e6fa68866ed4d87fba40b986c
priscadq <priscadq@gmail.com>
2018-01-13 14:18:37 -0300

    x fin pude sumar

--- Clase2/clase2-app/src/Header.js
+++ Clase2/clase2-app/src/Header.js
@@ -1,28 +1,33 @@
 import React, { Component } from "react";
-import HeaderImage from "./HeaderImage";
+
 import HeaderTitle from "./HeaderTitle";
+import HeaderImage from "./HeaderImage";
 
 class Header extends Component {
   constructor(props) {
     super();
+
     this.state = {
-      showImage: true
+      showImage: true,
+      counter: 0
     };
   }
 
   handleClick = () => {
-    const { showImage } = this.state;
+    const { showImage, counter } = this.state;
     this.setState({
-      showImage: !showImage
+      showImage: !showImage,
+      counter: counter + 1
     });
   };
 
   render() {
-    const { showImage } = this.state;
+    const { showImage, counter } = this.state;
+
     return (
       <header className="App-header" onClick={this.handleClick}>
         {showImage ? <HeaderImage /> : null}
-
+        {<h3>{counter}</h3>}
         <HeaderTitle />
       </header>
     );
--- Clase2/clase2-app/src/Header/index.js
+++
@@ -1,3 +0,0 @@
-import React, { Component } from "react";
-import Student from "./Student";
-import Header from "./Header";

53308f93434d3badc561bc9b41110216095453e7
priscadq <priscadq@gmail.com>
2018-02-24 12:47:07 -0300

    I REALLY LIKE THIS!

--- Clase4/marketplace-master/src/containers/home/ProductList.js
+++ Clase4/marketplace-master/src/containers/home/ProductList.js
@@ -1,28 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import ProductItem from './ProductItem';
 
-export default class ProductList extends Component {
-  constructor(props) {
-    super(props);
+const ProductList = (props) => (
 
+  <div className="row">
+  {
+    props.products.map(item => <ProductItem key={item.id} product={item} />)
   }
-  render() {
-    // Hacemos un mockup de productos en memoria, luego los traeremos de nuestra API
-    // const products = new Array(7).fill(
-    //   { 
-    //     imageUrl: 'http://placehold.it/700x400', 
-    //     title: 'Project One', 
-    //     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur eum quasi sapiente nesciunt? Voluptatibus sit, repellat sequi itaque deserunt, dolores in, nesciunt, illum tempora ex quae? Nihil, dolorem!' 
-    //   }
-    // );
-    
-    return (
-      <div className="row">
-        {
-          this.props.products.map((item, index) => <ProductItem key={index} product={item} />)
-        }
-      </div>
-    );
-  }
-}
+  </div>
+)
+
+
+export default ProductList;
